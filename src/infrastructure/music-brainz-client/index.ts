edc2327f2235f9d73bf100ea1e324b0e752b7956
Vitaliy Levkin <valevkin@avito.ru>
2024-04-21 23:13:30 +0700

    search

---
+++ src/infrastructure/music-brainz-client/index.ts
@@ -0,0 +1,112 @@
+const MUSIC_BRAINZ_URL = "https://musicbrainz.eu/ws/2",
+  MUSIC_BRAINZ_RECORDINGS_URL = "/recording",
+  MUSIC_BRAINZ_BASE_SEARCH_PARAMS = {
+    fmt: "json",
+  };
+
+export type RecordingsResponse = {
+  created: string;
+  count: number;
+  offset: number;
+  recordings: Array<{
+    id: string;
+    score: number;
+    title: string;
+    length?: number;
+    video: any;
+    "artist-credit": Array<{
+      name: string;
+      artist: {
+        id: string;
+        name: string;
+        "sort-name": string;
+        aliases?: Array<{
+          "sort-name": string;
+          name: string;
+          locale: any;
+          type?: string;
+          primary: any;
+          "begin-date": any;
+          "end-date": any;
+          "type-id"?: string;
+        }>;
+        disambiguation?: string;
+      };
+      joinphrase?: string;
+    }>;
+    "first-release-date": string;
+    releases: Array<{
+      id: string;
+      "status-id"?: string;
+      count: number;
+      title: string;
+      status?: string;
+      "artist-credit"?: Array<{
+        name: string;
+        artist: {
+          id: string;
+          name: string;
+          "sort-name": string;
+          disambiguation?: string;
+        };
+        joinphrase?: string;
+      }>;
+      "release-group": {
+        id: string;
+        "type-id"?: string;
+        "primary-type-id"?: string;
+        title: string;
+        "primary-type"?: string;
+        "secondary-types"?: Array<string>;
+        "secondary-type-ids"?: Array<string>;
+      };
+      date: string;
+      country: string;
+      "release-events": Array<{
+        date: string;
+        area: {
+          id: string;
+          name: string;
+          "sort-name": string;
+          "iso-3166-1-codes": Array<string>;
+        };
+      }>;
+      "track-count": number;
+      media: Array<{
+        position: number;
+        format?: string;
+        track: Array<{
+          id: string;
+          number: string;
+          title: string;
+          length?: number;
+        }>;
+        "track-count": number;
+        "track-offset": number;
+      }>;
+    }>;
+    isrcs?: Array<string>;
+    disambiguation?: string;
+    tags?: Array<{
+      count: number;
+      name: string;
+    }>;
+  }>;
+};
+
+export class MusicBrainzClient {
+  constructor(private readonly baseUrl = MUSIC_BRAINZ_URL) {}
+
+  async getRecordings(query: string): Promise<RecordingsResponse> {
+    const queryParams = new URLSearchParams({
+      ...MUSIC_BRAINZ_BASE_SEARCH_PARAMS,
+      query: query,
+    });
+
+    const response = await fetch(
+      `${this.baseUrl}${MUSIC_BRAINZ_RECORDINGS_URL}?${queryParams}`,
+    );
+
+    return await response.json();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { createServer } from "node:http";
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { createSchema, createYoga } from "graphql-yoga";
+import {
+  Recording,
+  ReleaseStatus,
+  SearchRecordingsInput,
+} from "./infrastructure/generated/graphql/types";
+import { MusicBrainzClient } from "./infrastructure/music-brainz-client";
 
 const typeDefs = readFileSync(
   resolve(__dirname, "../schema/graphql/schema.graphql"),
@@ -11,7 +17,86 @@ const typeDefs = readFileSync(
 const schema = createSchema({
   typeDefs,
   resolvers: {
-    Query: {},
+    Query: {
+      async searchRecordings(
+        _,
+        { input }: { input: SearchRecordingsInput },
+      ): Promise<Recording[]> {
+        console.log(input);
+        try {
+          const musicBrainzClient = new MusicBrainzClient();
+          const escapeLucene = (input: string) => {
+            const escaped = input.replace(
+              /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g,
+              "\\$&",
+            );
+            const joined = escaped.split(" ").join(" * ");
+            return joined;
+          };
+          const escapedQuery = escapeLucene(input.query);
+          const processedQuery = `"${escapedQuery}"~0.9 AND "${escapedQuery}"~7`;
+
+          const recordingsDto =
+            await musicBrainzClient.getRecordings(processedQuery);
+
+          console.log(JSON.stringify(recordingsDto, null, 2));
+
+          return recordingsDto.recordings.map((recordingDto) => ({
+            id: recordingDto.id,
+            title: recordingDto.title,
+            durationMs: recordingDto.length ?? 0,
+            firstReleaseDate: recordingDto["first-release-date"],
+            releases:
+              recordingDto.releases?.map((releaseDto) => ({
+                id: releaseDto.id,
+                title: releaseDto.title,
+                coverUrl: `https://coverartarchive.org/release/${releaseDto.id}/front`,
+                status:
+                  {
+                    Official: ReleaseStatus.Official,
+                    Promotion: ReleaseStatus.Promotion,
+                    Bootleg: ReleaseStatus.Bootleg,
+                    Withdrawn: ReleaseStatus.Withdrawn,
+                    PseudoRelease: ReleaseStatus.PseudoRelease,
+                    Cancelled: ReleaseStatus.Cancelled,
+                    "": ReleaseStatus.Unknown,
+                  }[releaseDto.status ?? ""] ?? ReleaseStatus.Unknown,
+                artists:
+                  releaseDto["artist-credit"]?.map((artistCreditDto) => ({
+                    name: artistCreditDto.name ?? "Неизвестен",
+                    artist: {
+                      id: artistCreditDto.artist.id,
+                      name: artistCreditDto.artist.name ?? "Неизвестен",
+                      sortName:
+                        artistCreditDto.artist["sort-name"] ??
+                        artistCreditDto.artist.name,
+                    },
+                    joinOn: artistCreditDto.joinphrase,
+                  })) ?? [],
+                date: releaseDto.date,
+              })) ?? [],
+            artists:
+              recordingDto["artist-credit"]?.map((artistCreditDto) => ({
+                name:
+                  artistCreditDto.name ??
+                  artistCreditDto.artist.name ??
+                  "Неизвестен",
+                artist: {
+                  id: artistCreditDto.artist.id,
+                  name: artistCreditDto.artist.name ?? "Неизвестен",
+                  sortName:
+                    artistCreditDto.artist["sort-name"] ??
+                    artistCreditDto.artist.name,
+                },
+                joinOn: artistCreditDto.joinphrase,
+              })) ?? [],
+          }));
+        } catch (e) {
+          console.error(e);
+          return [];
+        }
+      },
+    },
   },
 });
 
@@ -20,5 +105,5 @@ const yoga = createYoga({ schema, landingPage: false });
 const server = createServer(yoga);
 
 server.listen(4000, () => {
-  console.info("Server is running on http://localhost:4000");
+  console.info("Server is running on http://localhost:4000/graphql");
 });
